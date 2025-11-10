1b76f5a2bbbcea541e3ae00b494061117444c753
Артем Бичеров <jaineut@gmail.com>
2025-11-10 19:17:20 +0300

    Fix processing

--- src/processing.py
+++ src/processing.py
@@ -1,54 +1,48 @@
 import json
 import re
 from datetime import datetime
-from typing import Union
+from typing import Any
 
 
-def filter_by_state(input_customer_roster: str, state: str = "EXECUTED") -> Union[str, list]:
+def filter_by_state(input_customer_list: str, state: str = "EXECUTED") -> Any:
     """
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению.
     """
 
-    json_input_roster: str = input_customer_roster.replace("'", '"')
-    customer_roster = json.loads(json_input_roster)
-    filtered_customer_roster: list = [{}]
-    if not customer_roster:
+    json_input_list: str = input_customer_list.replace("'", '"')
+    customer_list = json.loads(json_input_list)
+    filtered_customer_list: list = [{}]
+    if not customer_list:
         return "Клиентская ведомость пуста"
-    for customer in customer_roster:
-        if customer not in customer_roster:
+    for customer in customer_list:
+        if customer not in customer_list:
             return "Клиентская ведомость пуста"
         elif customer.get("state") not in ["EXECUTED", "CANCELED"]:
             continue
         elif customer.get("state") in ["EXECUTED", "CANCELED"]:
-            filtered_customer_roster = list(filter(lambda key: key["state"] == state, customer_roster))
+            filtered_customer_list = list(filter(lambda key: key["state"] == state, customer_list))
         else:
             return "Ни одна из позиций не удовлетворяет запросу"
-    if not filtered_customer_roster:
+    if not filtered_customer_list:
         return "Ни одна из позиций не удовлетворяет запросу"
-    return filtered_customer_roster
+    return filtered_customer_list
 
 
-def sort_by_date(input_consumer_register: str, sort_order: str = "True") -> Union[str, list]:
+def sort_by_date(input_consumer_statement: str, sort_order: bool | None = True) -> Any:
     """
     Функция возвращает новый список словарей, отсортированный по дате (date).
     """
 
-    if sort_order == "True":
-        sort_order_bool = True
-    elif sort_order == "False":
-        sort_order_bool = False
-    elif sort_order == "":
-        sort_order_bool = True
-    else:
+    if not isinstance(sort_order, bool):
         return "Не задан порядок сортировки"
-    json_input_register: str = input_consumer_register.replace("'", '"')
-    consumer_register = json.loads(json_input_register)
-    valid_date_register = []
-    if not consumer_register:
+    json_input_statement: str = input_consumer_statement.replace("'", '"')
+    consumer_statement = json.loads(json_input_statement)
+    valid_date_statement = []
+    if not consumer_statement:
         return "Клиентская ведомость пуста"
-    for consumer in consumer_register:
-        if consumer not in consumer_register:
+    for consumer in consumer_statement:
+        if consumer not in consumer_statement:
             return "Клиентская ведомость пуста"
         elif consumer.get("date") is None:
             continue
@@ -57,15 +51,22 @@ def sort_by_date(input_consumer_register: str, sort_order: str = "True") -> Unio
                 datetime.strptime(consumer.get("date")[:10], "%Y-%m-%d").date()
             except (ValueError, TypeError):
                 continue
-            valid_date_register.append(consumer)
-    sorted_consumer_register = sorted(
-        valid_date_register,
+            valid_date_statement.append(consumer)
+    sorted_consumer_statement = sorted(
+        valid_date_statement,
         key=lambda idx: datetime.strptime(idx.get("date")[:10], "%Y-%m-%d").date(),
-        reverse=sort_order_bool,
+        reverse=sort_order,
     )
-    return sorted_consumer_register
+    return sorted_consumer_statement
 
 
 if __name__ == "__main__":
-    print(filter_by_state(input_customer_roster=input(), state=input()))
-    print(sort_by_date(input_consumer_register=input(), sort_order=input()))
+    print(filter_by_state(input_customer_list=input(), state=input()))
+    print(
+        sort_by_date(
+            input_consumer_statement=input(),
+            sort_order=(
+                True if (user_input := input().strip()) in ["True", ""] else (False if user_input == "False" else None)
+            ),
+        )
+    )
