5ef5bf6033af372c847c31af349aea6ac46f99cb
Артем Бичеров <jaineut@gmail.com>
2025-12-09 06:20:21 +0300

    Added loggers

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,15 @@
+import logging
+import os
 import re
+
+log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
+path_to_log = os.path.join(log_dir, "module_masks.log")
+masks_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(path_to_log, mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
+masks_logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: int | type[ValueError]) -> str:
@@ -6,11 +17,16 @@ def get_mask_card_number(card_number: int | type[ValueError]) -> str:
     Функция маскирует номер банковской карты.
     """
 
-    assert isinstance(card_number, int), "Введено некорректное значение карты."
+    masks_logger.info(f"Начало работы программы маскировки номера карты. Введено значение: {card_number}")
+    if not isinstance(card_number, int):
+        masks_logger.error("Введено некорректное значение карты.")
+        raise TypeError("Введено некорректное значение карты.")
     if str(card_number).isdigit() and (len(str(card_number)) == 16) and card_number is not type[ValueError]:
         mask_card_number = re.sub(r"^(\d{4})(\d{2})(\d{2})(\d{4})(\d{4})$", r"\1 \2** **** \5", str(card_number))
+        masks_logger.info(f"Номер карты успешно замаскирован: {mask_card_number}")
         return mask_card_number
     else:
+        masks_logger.error("Введено некорректное значение карты.")
         return "Введено некорректное значение карты."
 
 
@@ -19,11 +35,16 @@ def get_mask_account(account_number: int | type[ValueError]) -> str:
     Функция маскирует номер банковского счета.
     """
 
-    assert isinstance(account_number, int), "Введено некорректное значение счета."
+    masks_logger.info(f"Начало работы программы маскировки номера счета. Введено значение: {account_number}")
+    if not isinstance(account_number, int):
+        masks_logger.error("Введено некорректное значение счета.")
+        raise TypeError("Введено некорректное значение счета.")
     if str(account_number).isdigit() and (len(str(account_number)) == 20) and account_number is not type[ValueError]:
         mask_account = "**" + str(account_number)[-4:]
+        masks_logger.info(f"Номер счета успешно замаскирован: {mask_account}")
         return mask_account
     else:
+        masks_logger.error("Введено некорректное значение счета.")
         return "Введено некорректное значение счета."
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,42 @@
 import json
+import logging
 import os
+
+origin_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
+path_logfile = os.path.join(origin_dir, "module_utils.log")
+utils_logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(path_logfile, mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+utils_logger.addHandler(file_handler)
+utils_logger.setLevel(logging.DEBUG)
 
 
 def get_operations(file_path: str) -> list | str:
     """Функция возвращает список транзакций из файла operations.json"""
 
+    utils_logger.info("Начало работы программы чтения данных из файла.")
     if os.path.isfile(file_path):
         try:
             interim_file = open(file_path)
             interim_file.close()
+            utils_logger.info("Файл не занят.")
         except IOError:
+            utils_logger.error("Файл занят, попробуйте позже.")
             return "Файл занят, попробуйте позже."
         try:
             with open(file_path, "r", encoding="utf-8") as file:
                 raw_content = json.load(file)
+            utils_logger.info("Десериализация прошла успешно.")
         except json.JSONDecodeError:
+            utils_logger.error("Невозможно десериализовать данные.")
             return []
         if isinstance(raw_content, list):
+            utils_logger.info("Успешное прочтение списка.")
             return raw_content
         else:
+            utils_logger.error("Десериализованные данные - не список.")
             return []
     else:
+        utils_logger.error("Запрашиваемый файл не существует.")
         return []
